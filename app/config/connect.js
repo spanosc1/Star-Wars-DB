79497f85c463aedfbb4c71d9437b8199e29169e1
Chris Spanos <spanosc1@tcnj.edu>
2016-05-07 10:43:15 -0400

    jawsdb

--- app/config/connect.js
+++ app/config/connect.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '13748abc',
+	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'yw8amioux4x36kvt',
+	password: 'orh1zpd2u52t0cvp',
 	database: 'starwars'
 });
 
