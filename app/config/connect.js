189b6b96aeeb49632bc1626ba996366d03e413df
Chris Spanos <spanosc1@tcnj.edu>
2016-05-07 11:01:43 -0400

    jawsdb

--- app/config/connect.js
+++ app/config/connect.js
@@ -1,11 +1,23 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: 'yw8amioux4x36kvt',
-	password: 'orh1zpd2u52t0cvp',
-	database: 'ta3fnb4w38no1l01'
-});
+
+var source = {
+	localhost: {
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '13748abc',
+		database: 'starwars'
+	},
+	jawsDB: {
+		port: 3306,
+		host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'yw8amioux4x36kvt',
+		password: 'orh1zpd2u52t0cvp',
+		database: 'ta3fnb4w38no1l01'
+	}
+}
+
+var connection = mysql.createConnection(source.jawsDB);
 
 connection.connect(function(err) {
 	if (err)
@@ -17,3 +29,8 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
+host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'yw8amioux4x36kvt',
+	password: 'orh1zpd2u52t0cvp',
+	database: 'ta3fnb4w38no1l01'
